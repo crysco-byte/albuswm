ec83c3d6a3b32d62eaad0aafa4357368c593f1a1
crysco-byte <cryptoescor@protonmail.com>
2021-07-02 14:23:29 -0300

    removed unused keys/changed group names

--- src/bin/albuswm.rs
+++ src/bin/albuswm.rs
@@ -32,18 +32,9 @@ fn main() -> Result<()> {
         ([modkey], XK_Tab, cmd::lazy::layout_next()),
 
         ([modkey], XK_Return, spawn!("alacritty")),
-        ([modkey], XK_s, spawn!("qutebrowser")),
+        ([modkey], XK_o, spawn!("qutebrowser")),
         ([modkey], XK_q, spawn!("pkill", "Xorg")),
         ([modkey], XK_p, spawn!( "rofi", "-combi-modi", "drun,run,ssh","-theme", "slate", "-show", "combi", "-icon-theme", "Papirus", "-show-icons" )),
-
-        ([], XF86XK_MonBrightnessUp, spawn!("xbacklight", "-inc", "10")),
-        ([], XF86XK_MonBrightnessDown, spawn!("xbacklight", "-dec", "10")),
-        ([], XF86XK_AudioPrev, spawn!("playerctl", "previous")),
-        ([], XF86XK_AudioPlay, spawn!("playerctl", "play-pause")),
-        ([], XF86XK_AudioNext, spawn!("playerctl", "next")),
-        ([], XF86XK_AudioRaiseVolume, spawn!("amixer", "-q", "set", "Master", "5%+")),
-        ([], XF86XK_AudioLowerVolume, spawn!("amixer", "-q", "set", "Master", "5%-")),
-        ([], XF86XK_AudioMute, spawn!("amixer", "-q", "set", "Master", "toggle")),
     ];
 
     let padding = 20;
@@ -58,10 +49,10 @@ fn main() -> Result<()> {
         keys,
         shift,
         [
-            ([modkey], XK_a, "chrome", "stack"),
-            ([modkey], XK_s, "code", "stack"),
-            ([modkey], XK_d, "term", "tiled"),
-            ([modkey], XK_f, "misc", "tiled"),
+            ([modkey], XK_a, "alpha", "stack"),
+            ([modkey], XK_s, "beta", "stack"),
+            ([modkey], XK_d, "gamma", "tiled"),
+            ([modkey], XK_f, "delta", "tiled"),
         ]
     };
 
